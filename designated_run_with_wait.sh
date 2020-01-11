47c075c8863b7185683a39933d20f7a5797373fe
huizhen2014 <huizhen_2014@163.com>
2020-01-11 14:16:10 +0800

    updated

---
+++ designated_run_with_wait.sh
@@ -0,0 +1,17 @@
+##通过{}&使其同时进行
+count=0
+for i in $(seq 1 20)
+    do
+	count=$(( $count + 1))
+    {
+	echo "This the $i"
+	sleep 1
+    }&
+    
+    if [ $count -eq 4 ];then
+	wait
+	sleep 3
+	count=0
+	fi
+    done
+
